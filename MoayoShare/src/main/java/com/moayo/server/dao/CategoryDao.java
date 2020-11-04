a2ab22b71dc73096261287692ad84c51337c6734
gilwoong <smostr@gmail.com>
2020-11-04 22:44:09 +0900

    dao 관련 주석 작업 1차 완료.
    service 관련 주석 작업 예정.

--- MoayoShare/src/main/java/com/moayo/server/dao/CategoryDao.java
+++ MoayoShare/src/main/java/com/moayo/server/dao/CategoryDao.java
@@ -2,20 +2,76 @@ package com.moayo.server.dao;
 
 import com.moayo.server.model.CategoryModel;
 
-import java.util.List;
+/**
+ * MyBatis Mapper와 연결하는 interface.
+ *
+ * @author gilwoongkang
+ */
 
 public interface CategoryDao {
-    // SET FOREIGN_KEY_CHECKS=1;
+    /**
+     * 일반적 경우의 카테고리 insert.
+     * @param categoryModel 카테고리에 대한 model.
+     * @see CategoryModel
+     * @return row값 반환.
+     */
     long insertCategory(CategoryModel categoryModel);
-    long rootInsert(CategoryModel categoryModel);
+
+    /**
+     * 카테고리를 업데이트 한다.
+     * @param categoryModel 카테고리에 대한 model.
+     * @see CategoryModel
+     * @return 업데이트된 row갯수 반환.
+     */
     long updateCategory(CategoryModel categoryModel);
+
+    /**
+     * 카테고리를 제거한다. 모델 내부 카테고리 id를 가지고 제거한다.
+     * @param categoryModel 카테고리에 대한 model.
+     * @return 삭제된 row 갯수 반환.
+     */
     long deleteCategory(CategoryModel categoryModel);
+
+    /**
+     * 카테고리를 제거한다. dogam id와 category id 모두 일치하는 카테고리를 제거한다.
+     * @param id 카테고리에 대한 id
+     * @param dogamId 도감에 대한 id
+     * @return 삭제된 row 갯수 반환
+     */
     long deleteCategoryById(int id,int dogamId);
+
+    /**
+     * 카테고리를 제거한다. dogam id를 가지고 해당 도감과 관련된 모든 카테고리를 제거한다.
+     * @param dogamId 도감에 대한 id
+     * @return 삭제된 row 갯수 반환.
+     */
     long deleteCategoryByDogamId(int dogamId);
+
+    /**
+     * 카테고리를 Id로 검색해서 가져온다. dogam id와 category의 id를 이용한다.
+     * @param id 카테고리 id
+     * @param dogamId 도감의 id
+     * @return 카테고리 정보를 model에 매핑하여 리턴한다.
+     * @see CategoryModel
+     */
     CategoryModel getCategoryById(int id,int dogamId);
+
+    /**
+     * 도감과 관련된 모든 카테고리를 가져온다. dogam id를 입력해 관련 도감을 검색한다.
+     * @param dogamId 도감 Id.
+     * @return 카테고리들의 Array.
+     */
     CategoryModel[] getCategoryByDogamId(int dogamId);
 
-
+    /**
+     * 루트 카테고리 insert시 외래키 문제 해결을 위한 메소드.
+     * foreignkey의 영향을 받도록 변경한다.
+     */
     void foreignKeyON();
+
+    /**
+     * 루트 카테고리 insert시 외래키 문제 해결을 위한 메소드.
+     * foreignKey의 영향을 받지 않도록 변경한다.
+     */
     void foreignKeyOFF();
 }
--- MoayoShare/src/main/java/com/moayo/server/dao/DogamListDao.java
+++ MoayoShare/src/main/java/com/moayo/server/dao/DogamListDao.java
@@ -6,16 +6,79 @@ import org.springframework.stereotype.Repository;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * MyBatis Mapper와 연결하는 interface.
+ *
+ * @author gilwoongkang
+ */
 @Repository
 public interface DogamListDao {
+    /**
+     * 도감 정보를 insert한다.
+     * @param book insert하고자 하는 도감의 정보가 담긴 model.
+     * @see DogamListModel
+     * @return insert row.
+     */
     long insertDogam(DogamListModel book);
+
+    /**
+     * 도감 정보를 update 한다.
+     * @param book update하고자 하는 도감의 정보가 담긴 model.
+     * @return update row.
+     */
     long updateDogam(DogamListModel book);
+
+    /**
+     * 도감 정보를 model값 기준으로 삭제한다.
+     * @param book 삭제하고자 하는 도감의 정보가 담긴 model.
+     * @return delete row.
+     */
     long deleteDogamByModel(DogamListModel book);
+
+    /**
+     * 도감 정보를 도감 id기준으로 삭제한다.
+     * @param id 삭제하고자 하는 도감의 id
+     */
     void deleteDogamById(int id);
+
+    /**
+     * 도감 정보를 id값 기준으로 가져온다.
+     * @param id 가져오고자 하는 도감의 id값
+     * @return 가져온 도감의 model.
+     */
     DogamListModel getDogamById(int id);
+
+    /**
+     * 모든 도감 정보를 가져온다.
+     * @return 모든 도감의 model을 List에 담아 return.
+     */
     List<DogamListModel> getAllDogam();
+
+    /**
+     * 특정 도감 정보를 작성자 기준으로 가져온다.
+     * @param writer 작성자 nickname.
+     * @return 도감 model이 담긴 List.
+     */
     List<DogamListModel> getDogamByWriterName(String writer);
+
+    /**
+     * 설명란의 설명중 특정 키워드가 포함된 도감을 가져온다.
+     * @param description 찾고자 하는 키워드.
+     * @return 도감 model이 담긴 List.
+     */
     List<DogamListModel> getDogamByDescriptionSearch(String description);
+
+    /**
+     * 특정 도감의 like를 1 올린다.
+     * @param dogamId 좋아요 수치를 올리고자 하는 도감의 id.
+     * @return like row.
+     */
     int like(int dogamId);
+
+    /**
+     * 특정 도감의 like를 1내린다.
+     * @param dogamId 좋아요 수치를 내리고자 하는 도감의 id.
+     * @return dislike row.
+     */
     int disLike(int dogamId);
 }
